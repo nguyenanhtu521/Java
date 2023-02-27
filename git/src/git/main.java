a1be5e371346c24d396001ea5627cabd30ec150a
nguye <nguye@NAT>
2023-02-27 17:10:58 +0700

    Sua lan 1

--- git/src/git/main.java
+++ git/src/git/main.java
@@ -9,6 +9,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
+		System.out.println("Kết nối thành công");
 	}
 
 }
